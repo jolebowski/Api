287beff9e73e9276d9a49cb20dcc73c9374c445a
jordanm <jolebowski@gmail.com>
2020-04-13 18:25:42 +0200

    add memebers file et update index file

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const members = require('./members')
+
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World !</h1>')
-})
+//Get all members
+app.get('/api/members', (req, res) => res.json(members))
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`server is listenning in port ${PORT}`))
---
+++ members.js
@@ -0,0 +1,17 @@
+const members = [{
+    id: 1,
+    name: 'Jordan Moreira',
+    email: 'jolebowski@gmail.com',
+    status: 'active'
+},{
+    id:2,
+    name: 'Jordy Moreira',
+    email:'Jo@gmail.com',
+    status: 'inactive'
+},{
+    id:3,
+    name: 'Kevin Moreira',
+    email: 'keke@gmail;com',
+    status: 'active',
+}]
+module.exports = members
