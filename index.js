a9178542a32713b6e9aba94f4da4df41d4a6f128
jordanm <jolebowski@gmail.com>
2020-04-28 01:29:14 +0200

    install uuid package and a create a router for a post

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app = express()
   
 app.use(logger('dev'))
 
+// Body Parse Middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
 app.use("/api/members", require('./routes/members'))
 
 const PORT = process.env.PORT || 3000
--- routes/members.js
+++ routes/members.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const uuid = require('uuid')
 const router = express.Router()
 const members = require('../members')
 
@@ -15,5 +16,19 @@ router.get('/:id', (req,res) => {
         res.status(400).json({msg: `Member with id number ${req.params.id} not found`})
     }
 })
+//Create Member 
+router.post('/', (req, res) => {
+    const newMember = {
+        id: uuid.v4(),
+        name: req.body.name,
+        email: req.body.email,
+        status: 'active'
+    }
+    if(!newMember.name || !newMember.email){
+        return res.status(400).json({msg: "Please include  a name a email"})
+    }
+    members.push(newMember)
+    res.json(members)
+});
 
 module.exports = router
