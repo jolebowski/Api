8120b878c8380a291f263f1da035b0dc0b3b9670
jordanm <jolebowski@gmail.com>
2020-04-28 02:18:09 +0200

    update and delete a member

--- routes/members.js
+++ routes/members.js
@@ -16,7 +16,7 @@ router.get('/:id', (req,res) => {
         res.status(400).json({msg: `Member with id number ${req.params.id} not found`})
     }
 })
-//Create Member 
+//Create member 
 router.post('/', (req, res) => {
     const newMember = {
         id: uuid.v4(),
@@ -30,5 +30,30 @@ router.post('/', (req, res) => {
     members.push(newMember)
     res.json(members)
 });
-
+//update memeber
+router.put('/:id', (req,res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id))
+    if(found){
+        const updateMember = req.body
+        members.forEach(member => {
+            if(member.id === parseInt(req.params.id)){
+                member.name = updateMember.name ? updateMember.name : member.name
+                member.email = updateMember.email ? updateMember.email : member.email
+                res.json({msg: 'Member updated', member})
+            }
+        })
+    }else{
+        res.status(400).json({msg: `Member with id number ${req.params.id} not found`})
+    }
+})
+//delete member
+router.delete('/:id', (req,res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id))
+    if(found){
+        res.json({msg: "member deleted", 
+        members: members.filter(member => member.id !== parseInt(req.params.id))})
+    }else{
+        res.status(400).json({msg: `Member with id number ${req.params.id} not found`})
+    }
+})
 module.exports = router
